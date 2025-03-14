2c123ecd962e457527858087afe182304d7e6a5c
Spigelsp1ke <pletikos@yandex.ru>
2025-03-15 00:07:56 +0300

    feat: added js code to render cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const places = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(cardItem, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
+  cardImage.src = cardItem.link;
+  cardImage.alt = cardItem.name;
+  cardTitle.textContent = cardItem.name;
+
+  deleteButton.addEventListener("click", function() {
+    deleteCard(cardElement)
+  });
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function addCard(cardItem) {
+  const cardElement = createCard(cardItem, deleteCard);
+  places.append(cardElement);
+};
+
+function renderCards() {
+  initialCards.forEach(addCard);
+}
+
+renderCards();
